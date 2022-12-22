1fd51f35919a113da411967d81f925395581b87d
Jonas Moreira <jonasmoreira076@gmail.com>
2022-12-21 22:00:40 -0300

    Redirecionando URL

--- app.py
+++ app.py
@@ -19,5 +19,9 @@ def user(name):
     else: 
         return redirect(url_for('guest', guest=name))
 
+@app.route("/google")
+def google():
+    return redirect("http://google.com")
+
 if __name__=='__main__':
     app.run(debug=True)
