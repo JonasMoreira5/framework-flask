451ff8648711aeb4bf9a2e0d2349ce1877ead35d
Jonas Moreira <jonasmoreira076@gmail.com>
2022-12-21 21:30:06 -0300

    bordy

--- app.py
+++ app.py
@@ -1,19 +1,3 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-
-def home():
-    return "Instalaçao do Flask"
-
-def teste():
-    return "<p> Testando 1 </p>"
-    
-def teste2():
-    return "<p> Testando 2 </p>"
-
-app.add_url_rule("/teste","teste", teste)
-app.add_url_rule("/teste2","teste2", teste2)
-
-if __name__ == "__main__":
-    app.run(debug=True)
